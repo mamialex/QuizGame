de11a3747a9dacb463147cf27b190c772d77c7d7
mamialex <69636750+mamialex@users.noreply.github.com>
2022-10-04 12:21:07 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,63 @@
+from concurrent.futures import thread
+import socket
+from _thread import *
+import sys
+
+server = "192.168.178.45"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+
+s.listen(2)
+print("Waiting for connection, server started.")
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), (int(str[1]))
+
+def make_pos(tup):
+    return str(tup[0]) + ",", (str(tup[1]))
+
+pos = [(0,0), (100,100)]
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
+    reply = ""
+    while True:
+        try:
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Recieved: ", data)
+                print("Sending", reply)
+
+            conn.sendall(str.encode(make_pos(reply)))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+currentPlayer = 0
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn,))
+    currentPlayer += 1
